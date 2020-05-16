97974b647b315bc28020235fa8b11c8e42f25ad2
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 07:11:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-    document.addEventListener("DOMContentLoaded", function() {
-      console.log("The DOM has loaded");
-    });
-    
-    console.log("The DOM has loaded");
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  
+});
+     
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
