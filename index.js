201f7a0804073d2932737bb2e754737950f363f5
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 07:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  
+  document.getElementById('text').textContent = "This is really cool!";
 });
-     
-console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
